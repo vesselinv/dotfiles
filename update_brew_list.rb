fd551349261b83fa41db0fa76134ecedb4df8582
Veselin Vasilev <vesselinv@me.com>
2014-12-13 15:27:47 -0600

    update brew and brew cask lists

--- update_brew_list.rb
+++ update_brew_list.rb
@@ -1,10 +1,12 @@
-brew_list      = `brew list`.split("\n")
-brew_cask_list = `brew cask list`.split("\n")
+#!/usr/bin/env ruby
+
+brew_list = `brew list`.split("\n")
+cask_list = `brew cask list`.split("\n")
 
 current_text = File.read('brew')
 
 File.open('brew', "w") do |file|
   file.puts current_text.
     gsub(/^brew install (.*)$/,      "brew install #{brew_list.join(' ')}").
-    gsub(/^brew cask install (.*)$/, "brew cask install #{brew_cask_list.join(' ')}")
+    gsub(/^brew cask install (.*)$/, "brew cask install #{cask_list.join(' ')}")
 end
